4d3bbbd3bead2a6deb6ffcc8d558ea5915afbf8d
anboateng <yandyandg@gmail.com>
2020-10-28 17:09:46 -0700

    react joblisting app assignment

---
+++ src/components/home/JobDisplay.js
@@ -0,0 +1,97 @@
+// when each of the jobs are display, it will be displayed in this container
+
+import React, { Component } from 'react';
+
+class JobDisplay extends Component {
+  
+  render() {
+
+    const { job } = this.props;
+    
+    return (
+        <div className="job_details_area">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8">
+                    <div className="job_details_header">
+                        <div className="single_jobs white-bg d-flex justify-content-between">
+                            <div className="jobs_left d-flex align-items-center">
+                                <div className="thumb">
+                                    <img src="" alt="" />
+                                </div>
+                                <div className="jobs_conetent">
+                                    <a href="#"><h4>Software Engineer</h4></a>
+                                    <div className="links_locat d-flex align-items-center">
+                                        <div className="location">
+                                            <p> <i className="fa fa-map-marker"></i> California, USA</p>
+                                        </div>
+                                        <div className="location">
+                                            <p> <i className="fa fa-clock-o"></i> Part-time</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="jobs_right">
+                                <div className="apply_now">
+                                    <a className="heart_mark" href="#"> <i className="ti-heart"></i> </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="descript_wrap white-bg">
+                        <div className="single_wrap">
+                            <h4>Job description</h4>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing.</p>
+                            <p>Variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing.</p>
+                        </div>
+                        <div className="single_wrap">
+                            <h4>Responsibility</h4>
+                            <ul>
+                                <li>The applicants should have experience in the following areas.
+                                </li>
+                                <li>Have sound knowledge of commercial activities.</li>
+                                <li>Leadership, analytical, and problem-solving abilities.</li>
+                                <li>Should have vast knowledge in IAS/ IFRS, Company Act, Income Tax, VAT.</li>
+                            </ul>
+                        </div>
+                        <div className="single_wrap">
+                            <h4>Qualifications</h4>
+                            <ul>
+                                <li>The applicants should have experience in the following areas.
+                                </li>
+                                <li>Have sound knowledge of commercial activities.</li>
+                                <li>Leadership, analytical, and problem-solving abilities.</li>
+                                <li>Should have vast knowledge in IAS/ IFRS, Company Act, Income Tax, VAT.</li>
+                            </ul>
+                        </div>
+                        <div className="single_wrap">
+                            <h4>Benefits</h4>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                    <div className="job_sumary">
+                        <div className="summery_header">
+                            <h3>Job Summery</h3>
+                        </div>
+                        <div className="job_content">
+                            <ul>
+                                <li>Published on: <span>12 Nov, 2019</span></li>
+                                <li>Vacancy: <span></span></li>
+                                <li>Salary: <span>50k - 120k/y</span></li>
+                                <li>Location: <span>California, USA</span></li>
+                                <li>Job Nature: <span></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+  }
+}
+
+
+export default JobDisplay;
